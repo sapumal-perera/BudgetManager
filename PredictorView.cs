78c856eac4e5ed1d8d328c49135379f8a3d6ee70
Sapumal <isurusapumal100@gmail.com>
2022-03-20 13:55:54 +0530

    data store changes

---
+++ PredictorView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BudgetManager
+{
+    public partial class PredictorView : Form
+    {
+        public PredictorView()
+        {
+            InitializeComponent();
+        }
+
+        private void onPredictValue(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
